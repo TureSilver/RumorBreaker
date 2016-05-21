48f7660ce0f6004e207efa89d036509b3de27d9c
Dave Dai <jixiang.dai@hpe.com>
2016-05-21 18:59:33 +0800

    send analyze request

--- app/main-controller.js
+++ app/main-controller.js
@@ -1,10 +1,18 @@
-angular.module('my-app').controller('main-controller', function($scope, $http) {
+angular.module('my-app').controller('main-controller', function($scope, $window, $http) {
   var host= 'localhost';
   var analyzeUrl = '/1/api/sync/analyzesentiment/v1';
 
 
 	$scope.name = 'This is a rumor breaker';
 	$scope.submit = function() {
-		console.log($scope.inputText);
-	};
+    var encoded = $window.encodeURI($scope.inputText);
+    $http.post('http://localhost' + analyzeUrl, {
+      text: encoded,
+      language: 'chi'
+    }).then(function() {
+      console.log('done');
+    }, function(e) {
+      console.log('[Error]' + e);
+    });
+};
 });
