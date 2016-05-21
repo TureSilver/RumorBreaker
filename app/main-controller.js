705e08fcfe9d4535519843478f3bda125b62922b
Dave Dai <jixiang.dai@hpe.com>
2016-05-21 18:49:50 +0800

    add input box

--- app/main-controller.js
+++ app/main-controller.js
@@ -1,3 +1,10 @@
-angular.module('my-app').controller('main-controller', function($scope) {
+angular.module('my-app').controller('main-controller', function($scope, $http) {
+  var host= 'localhost';
+  var analyzeUrl = '/1/api/sync/analyzesentiment/v1';
+
+
 	$scope.name = 'This is a rumor breaker';
+	$scope.submit = function() {
+		console.log($scope.inputText);
+	};
 });
