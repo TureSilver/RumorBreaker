872cff6dcd532f09e22ed7d778dea31c937bfbc3
Dave Dai <jixiang.dai@hpe.com>
2016-05-22 12:32:58 +0800

    minor refact

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,22 +1,33 @@
-angular.module('my-app').controller('main-controller', function($scope, $window, $http) {
+angular.module('my-app').controller('main-controller', function($scope, $window, $http, $q) {
   var host= 'http://localhost:8080';
   var analyzeUrl = '/api/analyzesentiment';
+  var findSimilarUrl = '/api/findsimilar';
   var apikey = '0ad13f9a-ee6a-4aa1-bd85-63d9a5832580';
 
 
 	$scope.name = 'This is a rumor breaker';
 	$scope.submit = function() {
-    var encoded = $window.encodeURI($scope.inputText);
+    findSimilar($scope.inputText);
+  };
 
-    var url = host + analyzeUrl;
+  function findSimilar(inputText) {
+    var defer = $q.defer();
+
+    var encoded = $window.encodeURI(inputText);
+
+    var url = host + findSimilarUrl;
 
     url += '?language=' + 'chi';
     url += '&text=' + encoded;
+    url += '&indexes=articles';
 
     $http.post( url).then(function(data) {
+      var documents = data['documents'];
+      defer.resolve(documents);
       console.log('done');
     }, function(e) {
       console.log('[Error]' + e);
     });
-};
+    return defer.promise;
+  }
 });
