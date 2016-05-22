f5e068055888d5ae12be591a270599b99ea62a97
Dave Dai <jixiang.dai@hpe.com>
2016-05-22 14:00:21 +0800

    minor refact

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -14,10 +14,8 @@ angular.module('my-app').controller('main-controller', function($scope, $window,
         $scope.similar = documents;
         analyzeSentiment($scope.similar[0].content).then(function(data) {
           var score = data['aggregate']['score'];
-          if (score < 0) {
             $scope.probability = ((0.5 + Math.abs(score)) * 100).toString();
             $scope.probability = $scope.probability.substring(0,5) + '%';
-          }
         });
 
       }
